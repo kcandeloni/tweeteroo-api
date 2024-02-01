aa31379b8345cb2df4a003e5766e2a7be644552f
kcandeloni <kcandeloni@gmail.com>
2024-02-01 20:08:39 -0300

    correção da validação do id na criação de um twete

--- src/main/java/com/tweteroo/api/dtos/TweteDTO.java
+++ src/main/java/com/tweteroo/api/dtos/TweteDTO.java
@@ -1,6 +1,7 @@
 package com.tweteroo.api.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,6 +12,6 @@ public class TweteDTO {
     @Size(max = 280)
     private String text;
 
-    @NotBlank
+    @NotNull
     private Long userId;
 }
